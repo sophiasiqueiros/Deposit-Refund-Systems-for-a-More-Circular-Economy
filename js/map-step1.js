a95209b9df0b7094a8c3ff2b23ae95ead2a50981
Scott Gruber <scott@scottgruber.me>
2020-03-04 17:22:19 -0800

    First commit

--- js/map-step1.js
+++
@@ -1,9 +0,0 @@
-// Step One
-// Set location of map and zoom level
-var map = L.map('map').setView([34.069444, -118.445278], 11)
-
-// Load basemap and required attribution
-L.tileLayer(
-  'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; OpenStreetMap contributors'
-  }).addTo(map)
